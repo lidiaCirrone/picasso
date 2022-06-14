af9d3345365e83f3fc18e8eebf5a921f801945d4
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2022-06-14 16:09:58 +0200

    create gallery screen

--- screens/Gallery.tsx
+++ screens/Gallery.tsx
@@ -1,21 +1,55 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, ReactNode, useEffect, useState } from 'react';
 
 // components
-import { Image, View } from 'react-native';
+import { FlatList, Image, TouchableOpacity, View } from 'react-native';
 import { ParamListBase, RouteProp } from '@react-navigation/native';
+import { getObjFromLocalStorage, setLocalStorageObj } from '../utils/localStorage';
 interface GalleryProps {
    route: RouteProp<ParamListBase>;
 }
+
 const Gallery: FunctionComponent<GalleryProps> = (props): JSX.Element => {
+   const [state, setState] = useState({
+      images: []
+   })
+   useEffect(() => {
+      getImg()
+   }, [])
+   const getImg = async () => {
+      let myImages = await getObjFromLocalStorage('images')()
+      console.log('heeeey', myImages)
+      setState({
+         ...state,
+         images: myImages
+      })
+   }
+   const renderImages = (item: { index: number; item: { url: string } }, key: number): ReactNode => {
+      return (
+         <TouchableOpacity onLongPress={renderSelectedImage(item)}>
+            <Image
+               key={key}
+               source={{ uri: item.item.url }}
+               style={{ width: 100, height: 100, margin: 10 }}
+            />
+         </TouchableOpacity>
+
+      )
+   }
+   const renderSelectedImage = (item: { index: number }) => () => {
+      const index = item.index;
+      let obj = Object.assign({}, state)
+      obj.images.splice(index, 1)
+      setLocalStorageObj('images', obj.images)()
+      setState(obj)
+   }
    return (
       <>
-      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-         <Image
-            resizeMode="contain"
-            style={{ width: '100%', height: '100%' }}
-            source={{ uri: props?.route?.params?.url }}
-            />
-            </View>
+         <FlatList
+            contentContainerStyle={{ flexDirection: 'row' }}
+            data={state?.images}
+            style={{ padding: 10 }}
+            renderItem={renderImages}>
+         </FlatList>
       </>
    )
 }
