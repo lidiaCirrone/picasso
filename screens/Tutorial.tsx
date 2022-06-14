2287d9a5314a9591fa83f22545f84bb1bfe26add
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2022-06-14 17:52:09 +0200

    added function to set tutorial in ls

--- screens/Tutorial.tsx
+++ screens/Tutorial.tsx
@@ -7,17 +7,25 @@ import SliderTutorial from '../components/hookComponents/SliderTutorial';
 import { tutorialInfo } from '../utils/tutorialInfo';
 
 import { ParamListBase, RouteProp } from '@react-navigation/native';
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 
+import { setLocalStorageObj } from '../utils/localStorage'
 interface TutorialProps {
     route: RouteProp<ParamListBase>;
 }
 const Tutorial: FunctionComponent<TutorialProps> = (): JSX.Element => {
+    const okTutorial = () => {
+        setLocalStorageObj('tutorial', true)()
+    }
     return (
         <>
-            <View style={{ backgroundColor: 'white', flex: 1 }}>
-                <Text>Screen Tutorial</Text>
+            <View style={{ backgroundColor: '#007AFF', flex: 1 }}>
                 <SliderTutorial sliderArray={tutorialInfo} />
+                <View style={{ marginVertical: 20 }}>
+                    <TouchableOpacity onPress={okTutorial}>
+                        <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>I'm ready !</Text>
+                    </TouchableOpacity>
+                </View>
 
             </View>
         </>
